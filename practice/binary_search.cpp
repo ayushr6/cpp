1679439c40c51a52f60b2d061adfab82a276693e
ayushr6 <ayushranwa6@gmail.com>
2022-10-08 13:03:58 +0530

    practice/binary_search - int range optimization

--- practice/binary_search.cpp
+++ practice/binary_search.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int binary_search(int arr[], int start, int end, int key){
     if(start>end) return -1;
     else{
-        int mid = (start+end)/2;
+        int mid = start + (end-start)/2;
         if(arr[mid]==key) return mid;
         else if(arr[mid]>key) return binary_search(arr, start, mid-1, key);
         else binary_search(arr, mid+1, end, key);
